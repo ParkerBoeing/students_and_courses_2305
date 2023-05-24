3a43235396149afac7fc227268de06ed77f7e7ff
Parker Boeing <parker.boeing@gmail.com>
2023-05-24 10:11:39 -0600

    Feat: build out fully functional gradebook class

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,32 @@
 class Gradebook
-  
+  attr_reader :instructor, :courses, :list_all_students
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
+  end
+
+  def list_all_students
+    students = {}
+    courses.map do |course|
+      students[course] = course.students
+    end
+    students
+  end
+
+  def students_below(threshold)
+    below_threshold = []
+    courses.each do |course|
+      course.students.each do |student|
+        if student.grade < threshold
+          below_threshold << student
+        else
+        end
+      end
+    end
+    below_threshold
+  end
 end
