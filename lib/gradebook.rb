8af51be432ff6694d92692a7b7eb89ef67924cef
Parker Boeing <parker.boeing@gmail.com>
2023-05-24 10:26:29 -0600

    Feat: add list all grades and students in grade range methods

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -29,4 +29,30 @@ class Gradebook
     end
     below_threshold
   end
+
+  def all_grades
+    grade_map = {}
+    grades = []
+    courses.each do |course|
+      grades = []
+      course.students.each do |student| 
+        grades << student.grade
+      end
+      grade_map[course] = grades
+    end
+    grade_map
+  end
+
+  def students_in_range(min, max)
+  in_range = []
+    courses.each do |course|
+      course.students.each do |student|
+        if student.grade < max && student.grade > min
+        in_range << student
+        else
+        end
+      end
+    end
+  in_range
+  end
 end
