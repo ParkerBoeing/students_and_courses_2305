f23e29f1098d17178fb97f6980409e940fd10a4b
Parker Boeing <parker.boeing@gmail.com>
2023-05-24 09:22:11 -0600

    Feat: build out functioning student class

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
 class Student
- 
+ attr_reader :name, :age, :scores
+
+ def initialize(attributes)
+  @name = attributes[:name]
+  @age = attributes[:age]
+  @scores = []
+ end
+
+ def log_score(score)
+  @scores << score
+ end
+
+ def grade
+  scores.sum.to_f / scores.length
+ end
+
 end
