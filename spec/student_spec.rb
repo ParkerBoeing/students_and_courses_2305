5a2d3019e990991f7eed9a8eefee60b3f62fb907
Parker Boeing <parker.boeing@gmail.com>
2023-05-24 09:22:52 -0600

    Test: ensure functionality of student class

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,25 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  it 'exists' do
+    student = Student.new({name: "Morgan", age: 21})
+    expect(student).to be_a Student
+    expect(student.name).to eq "Morgan"
+    expect(student.age).to eq 21
+    expect(student.scores).to eq []
+  end
+
+  it 'can log scores' do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.scores).to eq [89, 78]
+  end
+
+  it 'can calculate the average grade' do
+    student = Student.new({name: "Morgan", age: 21})
+    student.log_score(89)
+    student.log_score(78)
+    expect(student.grade).to eq 83.5
+  end
 end
