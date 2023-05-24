099de1ae6399b33d60fe6a4c9df7ed8bb5462f2c
Parker Boeing <parker.boeing@gmail.com>
2023-05-24 09:27:18 -0600

    Feat: make functioning course class

--- lib/course.rb
+++ lib/course.rb
@@ -1,3 +1,23 @@
 class Course
+attr_reader :name, :capacity, :students
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+    @students = []
+    @full = false
+  end
+
+  def full?
+    if students.length >= capacity
+      @full = true
+      @full
+    else
+      @full
+    end
+  end
+
+  def enroll(student)
+    @students << student
+  end
 
 end
